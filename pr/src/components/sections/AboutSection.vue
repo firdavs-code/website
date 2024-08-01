5b35b04d711bc41fb97cdef5a645d1e4d3d4b964
firdavs-code <firdavsodiljonov580@gmail.com>
2024-08-01 07:47:17 -0400

    oka bold

--- pr/src/components/sections/AboutSection.vue
+++ pr/src/components/sections/AboutSection.vue
@@ -1,17 +1,15 @@
 <template>
-    <section class="h-screen w-full pt-32 px-20 flex gap-40" id="about">
-        <div class="text-5xl font-semibold text-stone-600 pt-2 flex flex-col gap-8 text-center">
-            <h1>Al-Xorazmiynig yonalishlari</h1>
-            <p class="text-3xl font-semibold max-w-4xl leading-relaxed">
+    <section class="h-screen w-full pt-20 px-8 md:px-20 flex flex-col md:flex-row gap-12 lg:gap-40" id="about">
+        <div class="font-semibold text-stone-600 pt-2 flex flex-col gap-8 text-center">
+            <h1 class="text-lg lg:text-4xl">Al-Xorazmiynig yonalishlari</h1>
+            <p class="text-sm lg:text-2xl font-semibold leading-relaxed w-auto">
                 Bizning academiyada hozida ingiliz tili, SAT, Matematika, hamda boshqa koplab xususiy
                 maktablardan prizident,<br> Al-Xorazmiy kabi maktablarg hamda, xususiy universitetlarga<br> kirishga
                 yordam beradi. Akademiyamizda oz kasbning mutahasislari dars berib boradi!
             </p>
         </div>
-        <div class="">
-
-            <img class="rounded-2xl max-w-3xl" src="@/assets/photos/c.jpg">
-
+        <div class="mb-auto text-center">
+            <img class="rounded-2xl mx-auto sm:max-md:w-96 w-[1400px]" src="@/assets/photos/c.jpg">
         </div>
     </section>
 </template>
