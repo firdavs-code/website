bc8864f883ed3a0d6e5cd2db1cf5af0e0c32a576
firdavs-code <firdavsodiljonov580@gmail.com>
2024-08-01 07:02:37 -0400

    oka qaren dabdala boldi

--- pr/src/components/sections/MainSection.vue
+++ pr/src/components/sections/MainSection.vue
@@ -1,11 +1,13 @@
 <template>
     <section class="h-screen flex items-center justify-center py-20 bg-cover
          bg-[url('@/assets/photos/a.jpg')] bg-no-repeat" id="main">
-
-        <div class="my-auto text-stone-300 flex flex-col items-center gap-28">
-            <h1 class="text-6xl mon-400">Xorazmiy Imzo Academiyasiga</h1>
-            <h2 class="text-4xl">XUSH KELIBSIZ!!! </h2>
+         <div class=" flex flex-col">
+              <div class=" gap-10 flex flex-col items-center ">
+            <h1 class="text-6xl 
+             mon-400 font-medium text-white">
+                Xorazmiy Imzo Academiyasiga</h1>
+            <h2 class="text-5xl font-medium text-white ">XUSH KELIBSIZ </h2>
         </div>
-
+         </div>
     </section>
 </template>
