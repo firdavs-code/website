a79151b96a0d14048ec26062dbf030159d121309
sintof <dcpersonf@gmail.com>
2024-08-01 15:05:31 +0500

    Create MainSection component

--- pr/src/App.vue
+++ pr/src/App.vue
@@ -2,14 +2,7 @@
     <div class="">
         <AppHeader></AppHeader>
         <main class="scroll-smooth ">
-            <section class="h-screen flex items-center justify-center py-20 bg-cover
-         bg-[url('@/assets/photos/a.jpg')] bg-no-repeat" id="main">
-                <div class="my-auto text-stone-300 flex flex-col items-center gap-28">
-                    <h1 class="text-6xl mon-400">Xorazmiy Imzo Academiyasiga</h1>
-                    <h2 class="text-4xl">XUSH KELIBSIZ!!! </h2>
-                </div>
-
-            </section>
+            <MainSection></MainSection>
             <section class="h-screen text-center py-20" id="about">
                 <div class="text-stone-500 text-3xl pb-10">
                     <h1>Al-Xorazmiynig yonalishlari</h1>
@@ -56,4 +49,5 @@
 
 <script setup>
 import AppHeader from './components/AppHeader.vue';
+import MainSection from './components/sections/MainSection.vue';
 </script>
---
+++ pr/src/components/sections/MainSection.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="h-screen flex items-center justify-center py-20 bg-cover
+         bg-[url('@/assets/photos/a.jpg')] bg-no-repeat" id="main">
+
+        <div class="my-auto text-stone-300 flex flex-col items-center gap-28">
+            <h1 class="text-6xl mon-400">Xorazmiy Imzo Academiyasiga</h1>
+            <h2 class="text-4xl">XUSH KELIBSIZ!!! </h2>
+        </div>
+
+    </section>
+</template>
