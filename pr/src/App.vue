da731d9805150f3b218556cfddb21f96b862abc7
sintof <dcpersonf@gmail.com>
2024-08-01 15:03:08 +0500

    Corrected photos

--- pr/src/App.vue
+++ pr/src/App.vue
@@ -3,7 +3,7 @@
         <AppHeader></AppHeader>
         <main class="scroll-smooth ">
             <section class="h-screen flex items-center justify-center py-20 bg-cover
-         bg-[url('@/assets/photos/ab.jpg')] bg-no-repeat" id="main">
+         bg-[url('@/assets/photos/a.jpg')] bg-no-repeat" id="main">
                 <div class="my-auto text-stone-300 flex flex-col items-center gap-28">
                     <h1 class="text-6xl mon-400">Xorazmiy Imzo Academiyasiga</h1>
                     <h2 class="text-4xl">XUSH KELIBSIZ!!! </h2>
@@ -21,7 +21,7 @@
                         yordam
                         beradi.<br>Academiyamizda oz kasbning ,utahasislari dars berib boradi
                     </p>
-                    <img class="size-2/5 rounded-xl" src="./assets/photos/pexels-karolina-grabowska-4219044.jpg">
+                    <img class="size-2/5 rounded-xl" src="./assets/photos/c.jpg">
 
                 </div>
 
