b6037aaf7967125f535c082be9c1fa95a93cebd7
sintof <dcpersonf@gmail.com>
2024-08-02 17:30:31 +0500

    Change

--- pr/src/App.vue
+++ pr/src/App.vue
@@ -16,4 +16,7 @@ import MainSection from './components/sections/MainSection.vue';
 import AboutSection from './components/sections/AboutSection.vue';
 import FounderSection from './components/sections/FounderSection.vue';
 import ContactSection from './components/sections/ContactSection.vue';
+
+
+
 </script>
--- pr/src/main.js
+++ pr/src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 
+import 'primeicons/primeicons.css'
+
 const app = createApp(App)
 
 app.use(router)
