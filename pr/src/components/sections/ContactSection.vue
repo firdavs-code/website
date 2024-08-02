3f8aaaf30971f8edc5fc2543ccc58d19ce8f6680
firdavs-code <firdavsodiljonov580@gmail.com>
2024-08-02 09:01:20 -0400

    sasdghjn

--- pr/src/components/sections/ContactSection.vue
+++ pr/src/components/sections/ContactSection.vue
@@ -1,3 +1,7 @@
 <template>
+    <div v-animateonscroll="{ enterClass: 'animate-zoomin', leaveClass: 'animate-fadeout' }" class="flex bg-white text-black shadow-lg justify-center items-center h-40 w-40 sm:h-60 sm:w-60 rounded-2xl animate-duration-1000">
+    <span class="text-3xl font-bold">zoom</span>
+</div>
+
     <section class="h-screen text-center py-20" id="contact">Contact</section>
 </template>
