a45d1068268554f199ecb31e83d76d922d04556f
firdavs-code <firdavsodiljonov580@gmail.com>
2024-08-02 07:19:38 -0400

    mana

--- pr/src/components/sections/MainSection.vue
+++ pr/src/components/sections/MainSection.vue
@@ -1,13 +1,14 @@
 <template>
     <section class="h-screen flex items-center justify-center py-20 bg-cover
-         bg-[url('@/assets/photos/a.jpg')] bg-no-repeat" id="main">
+         bg-no-repeat" id="main">
         <div class=" flex flex-col">
             <div class=" gap-10 flex flex-col items-center ">
-                <h1 class="text-6xl 
-             mon-400 font-medium text-white">
-                    Xorazmiy Imzo Academiyasiga</h1>
-                <h2 class="text-5xl font-medium text-white ">XUSH KELIBSIZ </h2>
+                <h1 class="text-6xl leading-tight tracking-wider font-bold mon-400 text-center text-slate-950 gap-10">
+                    Al-XORAZMIY ACADEMIYASI <br> FARZANDINGIZ UCHUN <br> TOGRI TANLOV</h1>
             </div>
         </div>
     </section>
 </template>
+
+
+<!-- bg-[url('@/assets/photos/a.jpg')] -->
